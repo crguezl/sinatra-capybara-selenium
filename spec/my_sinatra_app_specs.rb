638c642e2483c7d776d7ef0188c9e05956fe38aa
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2014-11-05 16:39:38 +0000

    forgot to add files!

---
+++ spec/my_sinatra_app_specs.rb
@@ -0,0 +1,9 @@
+require_relative '../my_sinatra_app.rb' # this load the file you are testing
+require 'spec_helper.rb' # It will load the configuration you set in spec_helper.rb
+
+describe 'make API call to load path' do 
+  it "should load the home page" do
+    visit 'http://0.0.0.0:4567'
+    page.should have_content("sinatra")
+  end
+end
