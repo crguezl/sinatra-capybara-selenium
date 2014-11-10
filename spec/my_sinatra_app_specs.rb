c44b4f78a9347256f82b78bf2b930ad6446c77a3
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2014-11-10 11:51:44 +0000

    capybara include in local scope

--- spec/my_sinatra_app_specs.rb
+++ spec/my_sinatra_app_specs.rb
@@ -1,9 +1,19 @@
-require_relative '../my_sinatra_app.rb' 
-require 'spec_helper.rb' 
+require 'capybara' # loading capybara
+require 'capybara/dsl'
+Capybara.default_driver = :selenium 
+
+ENV['RACK_ENV'] = 'test'
+
+RSpec.configure do |config|
+  config.include Capybara::DSL
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+  config.order = :random
+end
 
 describe 'make API call to load path' do 
   it "should load the home page" do
-    visit 'http://0.0.0.0:4567'
+    visit 'http://localhost:4567'
     expect(page).to have_content("Sinatra")
   end
 end
